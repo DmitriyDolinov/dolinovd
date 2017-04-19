966c8c17fca7cb97075de85476177d1b68d17d8e
DmitriyDolinov <dolinov91@gmail.com>
2017-04-19 08:29:04 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задачи части 001 урок1.
+ *@author dolinovd
+ *@since 18.04.2017
+*/
+
 public class Calculate {
+	/**
+	*Метод для тестирования
+	*param args - args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitriy Dolinov (mailto:dolinov91@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
