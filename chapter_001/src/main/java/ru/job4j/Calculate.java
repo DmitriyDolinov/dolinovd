5f5344f09de1ef253a8c83949381c4f768a34c1c
DmitriyDolinov <dolinov91@gmail.com>
2017-04-20 08:41:00 +0300

    task 4123

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ *Class Calculate решение задачи части 001 урок1.
+ *@author dolinovd
+ *@since 18.04.2017
+ *@version 1
+*/
+
+	public class Calculate {
+		/**
+		* Конструктор, выврл строки в консоль.
+		* @param arg - arg.
+		*/
+		public static void main(String[] arg) {
+			System.out.println("Hello World");
+		}
+	}
